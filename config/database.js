41e2a76f855b5db075a9cfb45ea3e3c94a87f1aa
Andy Smith <work@andysmith.is>
2021-11-09 15:50:30 +1100

    local db setup

--- config/database.js
+++ config/database.js
@@ -4,16 +4,38 @@ module.exports = ({ env }) => ({
     default: {
       connector: "bookshelf",
       settings: {
-        client: "postgres",
+        client: env("DATABASE_CLIENT", "postgres"),
         host: env("DATABASE_HOST", "127.0.0.1"),
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "eadee_strapi"),
         username: env("DATABASE_USERNAME", ""),
         password: env("DATABASE_PASSWORD", ""),
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
       options: {
         ssl: false,
+        seNullAsDefault: true,
       },
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "bookshelf",
+//       settings: {
+//         client: "postgres",
+//         host: env("DATABASE_HOST", "127.0.0.1"),
+//         port: env.int("DATABASE_PORT", 5432),
+//         database: env("DATABASE_NAME", "eadee_strapi"),
+//         username: env("DATABASE_USERNAME", ""),
+//         password: env("DATABASE_PASSWORD", ""),
+//       },
+//       options: {
+//         ssl: false,
+//       },
+//     },
+//   },
+// });
--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://cms.eadee.co",
+  // url: "https://cms.eadee.co",
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "5c304435b6f8db0e0f198dd3a0a2340c"),
