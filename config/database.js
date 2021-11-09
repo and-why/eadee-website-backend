052512e1651f34ace9e95afb9de6200cd8ebd120
Andy Smith <work@andysmith.is>
2021-11-09 16:15:37 +1100

    fallback change

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: env("DATABASE_CLIENT", "postgres"),
         host: env("DATABASE_HOST", "127.0.0.1"),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "eadee_strapi"),
+        database: env("DATABASE_NAME", "strapi"),
         username: env("DATABASE_USERNAME", ""),
         password: env("DATABASE_PASSWORD", ""),
         filename: env("DATABASE_FILENAME", ".tmp/data.db"),
