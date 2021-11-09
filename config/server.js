6b4e9740fc6f83bfbf589b8e071a25dbb60de118
Andy Smith <work@andysmith.is>
2021-11-09 11:12:18 +1100

    added subdomain

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "https://api.example.com",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '5c304435b6f8db0e0f198dd3a0a2340c'),
+      secret: env("ADMIN_JWT_SECRET", "5c304435b6f8db0e0f198dd3a0a2340c"),
     },
   },
 });
