f09888877525de26d4ba924a8d4691e80256c022
Andy Smith <work@andysmith.is>
2021-11-10 11:34:15 +1100

    added footer

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,15 @@
-module.exports = ({ env }) => ({
-  upload: {
-    provider: "aws-s3",
-    providerOptions: {
-      accessKeyId: env("AWS_ACCESS_KEY_ID"),
-      secretAccessKey: env("AWS_ACCESS_SECRET"),
-      region: env("AWS_REGION"),
-      params: {
-        Bucket: env("AWS_BUCKET_NAME"),
+if (process.env.NODE_ENV === "production") {
+  module.exports = ({ env }) => ({
+    upload: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        params: {
+          Bucket: env("AWS_BUCKET_NAME"),
+        },
       },
     },
-  },
-});
+  });
+}
